520de6be42c068143a36fbcb04bb049506f60b6f
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-01 17:33:14 +0000

    Automatically backed up by Learn

--- lib/nested_arrays.rb
+++ lib/nested_arrays.rb
@@ -1,21 +1,3 @@
-music_library = [
-  ["Adele",
-    ["19",
-      ["Day Dreamer", "Best for Last"]
-    ],
-    ["21",
-      ["Rolling in the Deep", "Rumor Has It"]
-    ]
-  ],
-  ["Beyonce",
-    ["4",
-      ["1 + 1", "Countdown"]
-    ],
-    ["Beyonce",
-      ["Haunted", "Pretty Hurts"]
-    ]
-  ]
-]
 
 music_library = [
   ["Adele",
